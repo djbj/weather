97efc9108f909f20aaa1646e299ef8710992fe93
Bjorn <bjossijohnson@gmail.com>
2017-11-23 14:57:49 +0100

    Plenty added

--- code/script.js
+++ code/script.js
@@ -3,7 +3,7 @@
 fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=a345d6bdc7e76fdc9f5de496e9687533")
 .then(
   (response) => {
-    console.log(response)
+    //console.log(response)
     return response.json()
   }).then((json) => {
     console.log(json)
@@ -12,5 +12,35 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
     document.getElementById("cityHeading").innerHTML = json.name
     document.getElementById("celciusCondition").innerHTML = json.main.temp_max + "°, " + json.weather[0].description
     console.log(json)
+    console.log(json.weather[0].icon)
+    const urlWeatherImage = "url(" + "'" + "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png" + "')"
+    console.log(urlWeatherImage)
+    // <img src="smiley.gif">
+    //const imageGenerator = "<img src=\""+urlWeatherImage+"\">"
+    //console.log(imageGenerator)
+    // document.getElementById("weatherImage").innerHTML = imageGenerator
+    //console.log(urlWeatherImage)
+    //const tag = document.getElementsByTagName("h3")[0].style.background-image: url("urlWeatherImage");
+    document.body.style.backgroundImage = urlWeatherImage
+
+    const sunriseToday = new Date(json.sys.sunrise * 1000);
+    const sunsetToday = new Date(json.sys.sunset * 1000)
+
+    document.getElementById("sunrise").innerHTML = "Sunrise today: " + sunriseToday
+    document.getElementById("sunset").innerHTML = "Sunset today: " + sunsetToday
+    // document.body.style.backgroundImage = "url('img_tree.png')";
+    // console.log(tag)
+    // console.log("Sunrise in milliseconds": json.sys.sunrise * 1000))
+
+
+    console.log(sunriseToday.getHours() + ":" + sunriseToday.getMinutes())
+    console.log(sunsetToday)
+    // document.write(myDate.toGMTString()+"<br>"+myDate.toLocaleString());
+
+
+
   }
+
+
+
 )
